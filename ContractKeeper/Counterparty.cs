8d859a8dc23bce19e3baad1ac0be67142e35bb29
Piligrem <kucherenkoIT@mail.ru>
2018-01-11 08:45:01 +0600

    1

---
+++ ContractKeeper/Counterparty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractKeeper
+{
+    public class Counterparty
+    {
+        public string Name { get; set; }
+        public OwnershipType OwnerType { get; set; }
+
+    }
+
+    public enum OwnershipType
+    {
+        LLC = 1, //ТОО
+        CORP, //АО
+        IBC, //Международная коммерческая компания
+        Ie //Индивидуальный предприниматель
+    }
+}
---
+++ ContractKeeper/DbConfigSection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ContractKeeper
+{
+    public class DbConnect : ConfigurationSection
+    {
+        [ConfigurationProperty("ConnectionString", DefaultValue = false, IsRequired = false)]
+        public string ConnectionString
+        {
+            get
+            {
+                object o = this["ConnectionString"];
+                return o as string;
+            }
+        }
+
+    }
+
+    public class DbConfigInstanceCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new DbConfigInstanceElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            //set to whatever Element Property you want to use for a key
+            return ((DbConfigInstanceElement)element).ConnectionString;
+        }
+    }
+
+    public class DbConfigInstanceElement : ConfigurationElement
+    {
+        //Make sure to set IsKey=true for property exposed as the GetElementKey above
+        [ConfigurationProperty("connectionstring", IsKey = true, IsRequired = true)]
+        public string ConnectionString
+        {
+            get { return (string)this["connectionstring"]; }
+            set { this["connectionstring"] = value; }
+        }
+
+    }
+}
+
